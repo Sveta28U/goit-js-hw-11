87ee740c27a9c649476bfdf5272b2e89748c7b5b
Sveta28U <152779991+Sveta28U@users.noreply.github.com>
2024-06-03 14:25:35 +0300

    commit 2 html

--- src/main.js
+++ src/main.js
@@ -2,15 +2,25 @@
 import iziToast from 'izitoast';
 // Додатковий імпорт стилів
 import 'izitoast/dist/css/iziToast.min.css';
+// Описаний у документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 import { getPhotos } from './js/pixabay-api';
 import { creatMarkup } from './js/render-functions';
 const form = document.querySelector('.search-form');
 const galleryList = document.querySelector('.gallery');
+const loader = document.querySelector('.loader');
 form.addEventListener('submit', onSubmit);
-
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 function onSubmit(event) {
   event.preventDefault();
+  galleryList.innerHTML = '';
+  showLoader();
   const searchQuery = event.currentTarget.elements.searchQuery.value.trim();
   getPhotos(searchQuery)
     .then(res => {
@@ -21,10 +31,21 @@ function onSubmit(event) {
             'Sorry, there are no images matching your search query. Please try again!',
         });
       }
-      console.log(res);
+
       galleryList.innerHTML = creatMarkup(res.hits);
+      lightbox.refresh();
     })
     .catch(error => {
       console.log(error);
+    })
+    .finally(() => {
+      hideLoader();
+      event.target.reset();
     });
+}
+function showLoader() {
+  loader.classList.remove('is-hidden');
+}
+function hideLoader() {
+  loader.classList.add('is-hidden');
 }
